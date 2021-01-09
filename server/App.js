d89bd81750c376b45cc0af757d11073f43689799
Yoshino Shuhei <s.yoshino.1113@gmail.com>
2021-01-09 22:08:16 +0900

    setup fetching data

--- server/App.js
+++ server/App.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const mongoose = require('mongoose')
+const schema = require('./schema/schema')
 const app = express()
 
+mongoose.connect('mongodb+srv://admin:test1234@cluster0.u7yzp.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+  console.log('connected')
+})
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
 app.listen(4000, () => {
   console.log('listening')
 })
